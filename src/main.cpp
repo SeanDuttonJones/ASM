5936a9e7f64682ddf358855b9b968af4be3759a2
Sean Dutton-Jones <sean4424@me.com>
2023-08-19 23:55:22 -0700

    Add Directive test

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <array>
 #include "Asm.hpp"
 #include "loader.hpp"
 #include <charconv>
@@ -6,6 +7,31 @@
 using namespace std;
 
 int main() {
+    array<unsigned char, 32> memory;
+    
+    // LOADING VALUE INTO MEMORY
+    float f = 3.14f;
+    unsigned char const *p = reinterpret_cast<unsigned char const *>(&f);
+    
+    for(std::size_t i = 0; i != sizeof(float); ++i) {
+        // std::printf("The byte #%zu is 0x%02X\n", i, p[i]);
+        memory[i] = p[i];
+    }
+
+    for(std::size_t i = 0; i < 32; i++) {
+        std::printf("MEMORY: byte #%zu is 0x%02X\n", i, memory[i]);
+    }
+
+    // READING VALUE FROM MEMORY
+    float k = 0;
+    unsigned char *kp = reinterpret_cast<unsigned char *>(&k);
+    for(std::size_t i = 0; i != sizeof(float); ++i) {
+        kp[i] = memory[i];
+        std::printf("The byte #%zu is 0x%02X\n", i, kp[i]);
+    }
+
+    cout << "K: " << k << endl;
+
     Asm stackMachine;
     
     Loader loader(stackMachine);
